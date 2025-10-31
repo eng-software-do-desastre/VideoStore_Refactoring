265b4e2f62384df3806ecac86b9cbdfb69d61332
erick <erickozaki@aquaporservicos.pt>
2025-10-31 23:08:23 +0000

    refactor: v1
    move method getAmount to Rental class

--- src/v1/Rental.java
+++ src/v1/Rental.java
@@ -20,4 +20,28 @@ public class Rental
 	{
 		return _movie;
 	}
+
+	public double getAmount()
+	{
+		double result = 0;
+	
+		// determine amounts for each line
+		switch (getMovie().getPriceCode())
+		{
+			case REGULAR:
+				result += 2;
+				if (getDaysRented() > 2)
+					result += (getDaysRented() - 2) * 1.5;
+				break;
+			case NEW_RELEASE:
+				result += getDaysRented() * 3;
+				break;
+			case CHILDRENS:
+				result += 1.5;
+				if (getDaysRented() > 3)
+					result += (getDaysRented() - 3) * 1.5;
+				break;
+		}
+		return result;
+	}
 }
